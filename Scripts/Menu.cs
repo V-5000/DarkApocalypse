c134f04d0008dcfc3d464b8c272945a817d6d8a2
Grey Code <58177990+Nominprogramer@users.noreply.github.com>
2021-04-23 15:56:25 +0530

    Add files via upload

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void play()
     {
-        
+        SceneManager.LoadScene(2);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Settings()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(0);
     }
 }
